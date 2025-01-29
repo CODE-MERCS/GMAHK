d3e730d742ae0353a4ce1eff9bb4b65be33f7af9
hokacristian <hokacristian@gmail.com>
2025-01-30 00:07:47 +0700

    fixxxx bisa

--- configs/prisma.js
+++ configs/prisma.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('@prisma/client'); // Import dari package yang benar
 const prisma = new PrismaClient();
 
 module.exports = prisma;
